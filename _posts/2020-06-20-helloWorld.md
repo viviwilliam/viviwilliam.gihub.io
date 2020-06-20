7a3f158218ebb112313912bddf23db825d4ef7b5
Ishihara <3109613630@qq.com>
2020-06-20 11:06:06 +0800

    first blog code

---
+++ _posts/2020-06-20-helloWorld.md
@@ -0,0 +1,7 @@
+---
+layout: post
+title: "helloworld"
+categories: 个人 非技术
+---
+
+测试一下中文博客，虽然我对此不报什么期望
---
+++ _scripts/renamefiles.py
@@ -0,0 +1,25 @@
+# Renames the post files to Jekyll-style.
+import os.path
+
+import utils
+import re
+
+#   https://viviwilliam.github.io/2008/02/09/
+
+def fix_file(path):
+    with open(path, 'r') as input:
+        print path
+
+        m = re.search('(.*)\.markdown', os.path.basename(path))
+        date = m.group(1)
+
+        content = input.read()
+        m = re.search('title: "(.*)"', content)
+        perm = utils.linkify(m.group(1))
+
+        # Save the file back out
+        with open('new/%s-%s.md' % (date, perm), 'w') as output:
+            output.write(content)
+
+utils.ensure_dir('new')
+utils.walk('_posts', fix_file, '.markdown')
